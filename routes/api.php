99731a0e0f31b749e6584bc87147ac3beac8cbb3
Melissa <mels1999@live.nl>
2020-07-16 22:54:29 +0200

    old_routes

--- routes/api.php
+++ routes/api.php
@@ -23,17 +23,17 @@ Route::get('logout','Api\AuthController@logout');
 Route::post('save_user_info','Api\AuthController@saveUserInfo')->middleware('jwtAuth');
 Route::get('userinfo','Api\AuthController@userinfo')->middleware('jwtAuth');
 
-//post
-Route::post('posts/create','Api\PostsController@create')->middleware('jwtAuth');
-Route::post('posts/delete','Api\PostsController@delete')->middleware('jwtAuth');
-Route::post('posts/update','Api\PostsController@update')->middleware('jwtAuth');
-Route::get('posts','Api\PostsController@posts')->middleware('jwtAuth');
+// //post
+// Route::post('posts/create','Api\PostsController@create')->middleware('jwtAuth');
+// Route::post('posts/delete','Api\PostsController@delete')->middleware('jwtAuth');
+// Route::post('posts/update','Api\PostsController@update')->middleware('jwtAuth');
+// Route::get('posts','Api\PostsController@posts')->middleware('jwtAuth');
 
-//comment
-Route::post('comments/create','Api\CommentsController@create')->middleware('jwtAuth');
-Route::post('comments/delete','Api\CommentsController@delete')->middleware('jwtAuth');
-Route::post('comments/update','Api\CommentsController@update')->middleware('jwtAuth');
-Route::post('posts/comments','Api\CommentsController@comments')->middleware('jwtAuth'); 
+// //comment
+// Route::post('comments/create','Api\CommentsController@create')->middleware('jwtAuth');
+// Route::post('comments/delete','Api\CommentsController@delete')->middleware('jwtAuth');
+// Route::post('comments/update','Api\CommentsController@update')->middleware('jwtAuth');
+// Route::post('posts/comments','Api\CommentsController@comments')->middleware('jwtAuth'); 
 
-//like
-Route::post('posts/like','Api\LikesController@like')->middleware('jwtAuth'); 
+// //like
+// Route::post('posts/like','Api\LikesController@like')->middleware('jwtAuth'); 
