77a0c9b5c524b49e660071d4263100cc1c317eef
Melissa <mels1999@live.nl>
2020-08-21 15:29:28 +0200

    version 2.1

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel v2
+                    Laravel v2.1
                 </div>
 
                 <div class="links">
